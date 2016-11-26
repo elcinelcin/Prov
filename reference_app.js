417952aeb87d4413bb21098cabafd9947b86f94c
kumbasar <vkumbasar@outlook.com>
2016-11-26 09:54:51 +0300

    add reference examples

---
+++ reference_app.js
@@ -0,0 +1,19 @@
+//pass by value
+function change(b) {
+  b = 2;
+}
+var a = 1;
+console.log(a);
+change(a);
+console.log(a);
+//
+//pass by reference
+function changeObj(d) {
+  d.prop1 = function() {};
+  d.prop2 = {};
+}
+var c = {};
+c.prop1 = {};
+console.log(c);
+changeObj(c);
+console.log(c);
